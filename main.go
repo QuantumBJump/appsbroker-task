cec55994aa19616d125bb2ad6e75d9555cf95566
Quinn Stevens <quantumjump451@gmail.com>
2021-12-15 22:29:02 +0000

    Move SQL interaction into handler

--- main.go
+++ main.go
@@ -63,6 +63,40 @@ func main() {
 
 	log.Printf("File contents: %s", content_string)
 
+	// Generate and display template
+	t, err := template.ParseFiles("template/index.html")
+	if err != nil {
+		log.Fatalf("Error parsing template: %+v", err)
+	}
+
+	tmpl = t
+
+	data = templateData{
+		Message:      "Hello, world!",
+		CloudStorage: content_string,
+	}
+
+	addr := defaultAddr
+	// $PORT environment variable is provided in the k8s deployment
+	if p := os.Getenv("PORT"); p != "" {
+		addr = ":" + p
+	}
+	log.Printf("Server listening on port %s", addr)
+
+	http.HandleFunc("/", home)
+
+	fs := http.FileServer(http.Dir("template/static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatalf("Server listening error: %+v", err)
+	}
+}
+
+// home responds to requests by rendering an html page
+func home(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Hello world! Received request: %s %s", r.Method, r.URL.Path)
+
 	// SQL interaction
 	dbUsername := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASS")
@@ -92,40 +126,9 @@ func main() {
 	}
 
 	db.Close()
-	// Generate and display template
-	t, err := template.ParseFiles("template/index.html")
-	if err != nil {
-		log.Fatalf("Error parsing template: %+v", err)
-	}
 
-	tmpl = t
+	data.CloudSQL = objects
 
-	data = templateData{
-		Message:      "Hello, world!",
-		CloudStorage: content_string,
-		CloudSQL:     objects,
-	}
-
-	addr := defaultAddr
-	// $PORT environment variable is provided in the k8s deployment
-	if p := os.Getenv("PORT"); p != "" {
-		addr = ":" + p
-	}
-	log.Printf("Server listening on port %s", addr)
-
-	http.HandleFunc("/", home)
-
-	fs := http.FileServer(http.Dir("template/static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		log.Fatalf("Server listening error: %+v", err)
-	}
-}
-
-// home responds to requests by rendering an html page
-func home(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Hello world! Received request: %s %s", r.Method, r.URL.Path)
 	if err := tmpl.Execute(w, data); err != nil {
 		msg := http.StatusText(http.StatusInternalServerError)
 		log.Printf("template.Execute: %v", err)
